2f58c7be7247854e6941a3b32598e962741787a8
charles nseke <nsekecharles@gmail.com>
2021-02-23 13:31:30 +0100

    little renaming

--- indexer.js
+++ indexer.js
@@ -4,15 +4,15 @@ const indexationRunner = require('./indexationRunner');
 const app = express()
 
 app.get('/indexWords', (req, res) => {
-    const wordsToIndexQuery = 'SELECT * FROM mots limit 5';
+    const wordsToIndexQuery = 'SELECT * FROM mots';
     indexationRunner.run(wordsToIndexQuery, 'words');
     res.send('Words indexation started...');
 });
 
 app.get('/indexTranslations', (req, res) => {
-    const wordsToIndexQuery = 'SELECT * FROM traductions';
-    indexationRunner.run(wordsToIndexQuery, 'translations');
-    res.send('Traductions indexation started...');
+    const translationsToIndexQuery = 'SELECT * FROM traductions';
+    indexationRunner.run(translationsToIndexQuery, 'translations');
+    res.send('Translations indexation started...');
 });
 
 app.listen(config.port, () => console.log(`Server running on port ${config.port}!`));
